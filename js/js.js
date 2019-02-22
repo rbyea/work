d94b77b7742113f2fcf0d55a11d2e2849312aa56
Egor <csgo.roby@gmail.com>
2019-02-22 14:45:59 +0300

    плавная прокрутко до якоря

--- js/js.js
+++ js/js.js
@@ -1,6 +1,6 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
 	// slider
-  $('.slider').slick({
+	$('.slider').slick({
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		arrows: false,
@@ -18,33 +18,58 @@ jQuery(document).ready(function($){
 	});
 
 
-	
+
 
 }); // end jquery
 
-jQuery_1_6(document).ready(function($){
+jQuery_1_6(document).ready(function ($) {
 	// zoom
-	jQuery_1_6('.slider .slider-item.zoom').addimagezoom({ 
+	jQuery_1_6('.slider .slider-item.zoom').addimagezoom({
 		zoomrange: [3, 10],
-		magnifiersize: [300,300],
+		magnifiersize: [300, 300],
 		magnifierpos: 'right',
 		cursorshade: true,
-		largeimage: '../img/slider/photo-slider.png' 
+		largeimage: '../img/slider/photo-slider.png'
 	})
 
 }); // end jquery_1_6
 
 
-	//rating js
-	(function(){
-		var container = document.querySelector('.rating');
-		var items = container.querySelectorAll('.rating-item')
-		container.onclick = function(e) {
-			if( ! e.target.classList.contains('active') ){
-				items.forEach(function(item){
-					item.classList.remove('active');
-				});
-				e.target.classList.add('active');
-			}
+//rating js
+(function () {
+	var container = document.querySelector('.rating');
+	var items = container.querySelectorAll('.rating-item')
+	container.onclick = function (e) {
+		if (!e.target.classList.contains('active')) {
+			items.forEach(function (item) {
+				item.classList.remove('active');
+			});
+			e.target.classList.add('active');
 		}
-	})();
+	}
+})();
+
+// прокрутка до якоря
+const anchors = [].slice.call(document.querySelectorAll('a[href*="#menu-one"], a[href*="#menu-two"], a[href*="#menu-three"], a[href*="#menu-four"], a[href*="#menu-five"], a[href*="#menu-six"]')),
+	animationTime = 300,
+	framesCount = 20;
+
+anchors.forEach(function (item) {
+	item.addEventListener('click', function (e) {
+		e.preventDefault();
+
+		let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top;
+
+		let scroller = setInterval(function () {
+
+			let scrollBy = coordY / framesCount;
+
+			if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+				window.scrollBy(0, scrollBy);
+			} else {
+				window.scrollTo(0, coordY);
+				clearInterval(scroller);
+			}
+		}, animationTime / framesCount);
+	});
+});
