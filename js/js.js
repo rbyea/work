819da47b527b57d32b3b2f9340b202c12c7e9cb8
optimist93 <optimist993@gmail.com>
2019-02-22 07:57:36 +0500

    зум-2

--- js/js.js
+++ js/js.js
@@ -1,26 +1,39 @@
-$(document).ready(function(){
-
+jQuery(document).ready(function($){
 	// slider
   $('.slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav'
-  });
-  $('.slider-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider',
-    centerMode: true,
-    centerPadding: '0px',
-    focusOnSelect: true,
-    vertical: true
-  });
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: true,
+		asNavFor: '.slider-nav'
+	});
+	$('.slider-nav').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		asNavFor: '.slider',
+		centerMode: true,
+		centerPadding: '0px',
+		focusOnSelect: true,
+		vertical: true
+	});
+
+
 	
-	$('a.zoom').easyZoom();
 
 }); // end jquery
+
+jQuery_1_6(document).ready(function($){
+	// zoom
+	jQuery_1_6('.slider .slider-item.zoom').addimagezoom({ 
+		zoomrange: [3, 10],
+		magnifiersize: [300,300],
+		magnifierpos: 'right',
+		cursorshade: true,
+		largeimage: '../img/slider/photo-slider.png' 
+	})
+
+}); // end jquery_1_6
+
 
 	//rating js
 	(function(){
