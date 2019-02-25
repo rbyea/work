d5df6ab237d358fede8d2bf1906f2a26bfe0785f
Egor <csgo.roby@gmail.com>
2019-02-25 03:59:11 +0300

    накинул адресса в мобильное меню

--- js/js.js
+++ js/js.js
@@ -90,7 +90,8 @@ $(document).ready(function(){
 
 //прилипающеее меню
 	var sticky = new Sticky('.sticky');
-
+	
+//всплывающее меню, через 1 минуту
 	$(".warning-close").hide();
 
 	$('.warning').fadeOut().delay(3000).fadeIn();
